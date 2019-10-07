5e2ac1b28102764917482434999c36ecbdaae61e
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-10-07 10:46:08 +0200

    Update Beer.cs

--- Ølspecialisten/Ølspecialisten.Core.Entity/Beer.cs
+++ Ølspecialisten/Ølspecialisten.Core.Entity/Beer.cs
@@ -6,5 +6,27 @@ namespace Ølspecialisten.Core.Entity
 {
     class Beer
     {
+
+        public double Pris { get; set; }
+        public string Beskrivelse { get; set; }
+        public Nationalitet Nation { get; set; }
+        public int Lager { get; set; }
+        public string Navn { get; set; }
+        public string BeskrivelseLang { get; set; }
+        public double Alkohol { get; set; }
+        public double Kapacitet { get; set; }
+
+        public enum Nationalitet
+        {
+            Dansk,
+            Norsk,
+            Svensk
+        }
+
+        public enum TypeØl
+        {
+            Lys,
+            Mørk
+        }
     }
 }
