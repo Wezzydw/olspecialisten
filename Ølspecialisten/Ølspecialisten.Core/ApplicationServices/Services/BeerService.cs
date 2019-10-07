e4238ed6236fc149ea21820b52e40b69de39ff6b
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-10-07 11:27:43 +0200

    Update BeerService.cs

--- Ølspecialisten/Ølspecialisten.Core/ApplicationServices/Services/BeerService.cs
+++ Ølspecialisten/Ølspecialisten.Core/ApplicationServices/Services/BeerService.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ølspecialisten.Core.DomainServices;
+using Ølspecialisten.Core.Entity;
 
 namespace Ølspecialisten.Core.ApplicationServices.Services
 {
-    class BeerService
+    public class BeerService : IBeerService
     {
+
+        private IBeerRepository _beerRepository;
+
+        public BeerService(IBeerRepository beerRepository)
+        {
+            _beerRepository = beerRepository;
+        }
+        public List<Beer> GetAllBeers()
+        {
+            return _beerRepository.GetAllBeers();
+        }
+
+        public void CreateBeer(Beer beer)
+        {
+            _beerRepository.CreateBeer(beer);
+        }
+
+        public void DeleteBeer(int id)
+        {
+            _beerRepository.DeleteBeer(id);
+        }
+
+        public void UpdateBeer(Beer beer)
+        {
+           _beerRepository.UpdateBeer(beer);
+        }
+
+        public List<Beer> GetBeersByType(Beer.TypeØl type)
+        {
+            return _beerRepository.GetBeersByType(type);
+        }
     }
 }
